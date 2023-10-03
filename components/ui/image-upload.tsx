e30c66c06ac225bb4ce1ffe8d9e7b39bdddd615c
Paweł <pawel_gnat@o2.pl>
2023-10-03 20:28:05 +0200

    feat: billboard components

---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+
+interface ImageUploadProps {
+	disabled?: boolean
+	onChange: (value: string) => void
+	onRemove: (value: string) => void
+	value: string[]
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({ disabled, onChange, onRemove, value }) => {
+	const [isMounted, setIsMounted] = useState(false)
+
+	useEffect(() => {
+		setIsMounted(true)
+	}, [])
+
+	const onUpload = (result: any) => {
+		onChange(result.info.secure_url)
+	}
+
+	if (!isMounted) {
+		return null
+	}
+
+	return (
+		<div>
+			<div className='mb-4 flex items-center gap-4'>
+				{value.map(url => (
+					<div
+						key={url}
+						className='relative w-[200px] h-[200px] rounded-md overflow-hidden'>
+						<div className='z-10 absolute top-2 right-2'>
+							<Button></Button>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default ImageUpload
